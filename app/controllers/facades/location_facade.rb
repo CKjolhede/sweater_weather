8ed3ec8ee87e19aeea6fc3c2c2efeb9d0372f384
Chris Kjolhede <ckjolhede@gmail.com>
2022-08-08 04:51:00 -0600

    add poros

--- app/controllers/facades/location_facade.rb
+++ app/controllers/facades/location_facade.rb
@@ -2,6 +2,7 @@ class LocationFacade
 
   def self.get_coords(location)
     json = LocationService.get_coords(location)
+    
     Location.new(json[:results][0][:locations][0][:latlong])
 
   # binding.pry
