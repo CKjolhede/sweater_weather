b49129dacc0029fd5e7baaa2c77870f9d089696d
Chris Kjolhede <ckjolhede@gmail.com>
2022-08-08 03:40:32 -0600

    create locations_facade and add params to location services

--- app/controllers/facades/locations_facade.rb
+++ app/controllers/facades/locations_facade.rb
@@ -1,3 +1,7 @@
 class LocationsFacade
-  
+
+  def self.get_coords(location)
+    json = LocationService.get_coords(location)
+    json[:results][0][:locations][0][:latlong]
+  end
 end
--- app/controllers/services/location_service.rb
+++ app/controllers/services/location_service.rb
@@ -1,3 +1,11 @@
 class LocationsServices
+
+  def self.get_coords(location)
+    response = location_conn.get("/geocoding/v1/address?location=#{location}")
+
+  response_body = JSON.parse(response.body, symbolize_names: true)
+
+  binding.pry
+  end
   
 end
