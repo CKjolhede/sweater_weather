0c591ea188deb6d0137b61c83da1f6f1304d6bff
Chris Kjolhede <ckjolhede@gmail.com>
2022-08-08 03:34:06 -0600

    create base services include hidden api keys

--- app/controllers/services/base_service.rb
+++ app/controllers/services/base_service.rb
@@ -1,10 +1,14 @@
 class BaseService
 
   def self.location_conn
-    Faraday.new('http://www.mapquestapi.com')
+    Faraday.new('http://www.mapquestapi.com') do |req|
+      req.params['key'] = ENV['location_api_key']
+    end
   end
 
   def self.forecast_conn
-     Faraday.new('https://api.openweathermap.org/data/2.5/onecall')
+     Faraday.new('https://api.openweathermap.org/data/2.5/onecall') do |rec|
+        req.params['appid'] = ENV['forecast_api_key']
+     end
   end
 end
