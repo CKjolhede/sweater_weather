1d98c932877f7abae26e3d95fe7d56a51505490c
Chris Kjolhede <ckjolhede@gmail.com>
2022-08-09 06:46:49 -0600

    create user routses

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
     namespace :v1 do
       resources :forecast, only: %i[index]
       resources :munchies, only: [:index]
+      resources :users, only: [:create]
     end
   end
 
