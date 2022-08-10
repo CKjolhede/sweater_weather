9b79781a6d3ed264c841d8a707e71c58eeff7483
Chris Kjolhede <ckjolhede@gmail.com>
2022-08-09 18:34:31 -0600

    user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+RSpec.describe "User", type: :model do
+
+    describe "validations" do
+    
+        it { should have_secure_password}
+        it { should have_secure_token}
+        
+        it {should validate_presence_of :email}
+        it {should validate_presence_of :password_digest}
+      end
+    end 
